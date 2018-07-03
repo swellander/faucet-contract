942d0bb3a682ab210451099f990d4d2c536dd64c
Sam Wellander <samwellander@gmail.com>
2018-07-03 15:58:57 -0400

    remove comments

--- Faucet.sol
+++ Faucet.sol
@@ -1,20 +1,14 @@
-// Version of Solidity compiler this program was written for
 pragma solidity ^0.4.19;
 
-// Our first contract is a faucet!
 contract Faucet2 {
 
-	// Give out ether to anyone who asks
 	function withdraw(uint withdraw_amount, uint key) public {
 
         require(key == 6);
-    	// Limit withdrawal amount
 
-    	// Send the amount to the address that requested it
     	msg.sender.transfer(withdraw_amount);
     }
 
-	// Accept any incoming amount
 	function () public payable {}
 
 }
